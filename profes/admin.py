4bad77296bb1fca63c1a50be4a84a0a6e230659c
Jesus Lucas <jelukas89@gmail.com>
2013-01-02 11:11:32 +0100

    Primer Commit

---
+++ profes/admin.py
@@ -0,0 +1,89 @@
+from django.contrib import admin
+from profes.models import Curso, Profesor, Posibilidad, Telefono, Tarea, Edicion, Categoria
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin import SimpleListFilter
+
+class RangoAlumnosListFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Rango Alumnos'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'alumnos_minimos'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('0-5', _('de 0 a 5')),
+            ('6-10', _('de 6 a 10')),
+            ('11-20', _('de 11 a 20')),
+            ('21-40', _('de 21 a 40')),
+            )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or 'other')
+        # to decide how to filter the queryset.
+        if self.value() == '0-5':
+            return queryset.filter(alumnos_minimos__lte=5)
+        if self.value() == '6-10':
+            return queryset.filter(alumnos_minimos__gte=6,alumnos_minimos__lte=10)
+        if self.value() == '11-20':
+            return queryset.filter(alumnos_minimos__gte=11,alumnos_minimos__lte=20)
+        if self.value() == '21-40':
+            return queryset.filter(alumnos_minimos__gte=21,alumnos_minimos__lte=40)
+
+
+
+class TelefonoInline(admin.TabularInline):
+    model = Telefono
+    extra = 1
+
+class TareaInline(admin.TabularInline):
+    model = Tarea
+    extra = 1
+
+class PosibilidadInline(admin.TabularInline):
+    model = Posibilidad
+    raw_id_fields = ('curso',)
+    extra = 1
+
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ['nombre','apellidos','email','provincia','ciudad']
+    inlines = [TelefonoInline,PosibilidadInline,TareaInline]
+
+class PosibilidadAdmin(admin.ModelAdmin):
+    list_display = ['curso','profesor','alumnos_minimos',]
+    search_fields = ('curso__titulo','profesor__nombre','profesor__apellidos',)
+    list_filter = [RangoAlumnosListFilter,]
+
+class CursoAdmin(admin.ModelAdmin):
+    search_fields = ('titulo','tipo',)
+    inlines = [PosibilidadInline,]
+
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+class EdicionAdmin(admin.ModelAdmin):
+    list_display = ['codigo','__unicode__','alumnos_minimos']
+
+
+
+
+
+
+admin.site.register(Curso,CursoAdmin)
+admin.site.register(Profesor,ProfesorAdmin)
+admin.site.register(Posibilidad,PosibilidadAdmin)
+admin.site.register(Edicion,EdicionAdmin)
+admin.site.register(Categoria,CategoriaAdmin)
---
+++ profes/models.py
@@ -0,0 +1,88 @@
+# -*- encoding: utf-8 -*-
+from django.db import models
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    pais = models.CharField(max_length=200,default='Spain')
+    provincia = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200)
+    email = models.CharField(max_length=250)
+    edad = models.IntegerField(blank=True,null=True)
+
+    def __unicode__(self):
+        return self.nombre + ' ' +  self.apellidos
+
+
+class Telefono(models.Model):
+    numero = models.CharField(max_length=30)
+    profesor = models.ForeignKey(Profesor,related_name='telefonos')
+
+    def __unicode__(self):
+        return unicode.encode(self.numero)
+
+
+
+TIPOS_CURSOS = (
+    ('online', 'Online'),
+    ('presencial', 'Presencial'),
+    ('semipresencial', 'Semipresencial'),
+    ('subvencionado', 'Subvencionado'),
+)
+
+class Categoria(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.codigo + ' - ' + self.nombre
+
+
+class Curso(models.Model):
+    numero = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+    posibilidades = models.ManyToManyField(Profesor,through='Posibilidad')
+    tipo = models.CharField(max_length=100,choices=TIPOS_CURSOS)
+    categoria = models.ForeignKey(Categoria,related_name='cursos')
+
+    def __unicode__(self):
+        return self.titulo
+
+
+class Tarea(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    mensaje = models.TextField()
+    profesor = models.ForeignKey(Profesor)
+
+    def __unicode__(self):
+        return str(self.created_at)
+
+
+
+class Posibilidad(models.Model):
+    modified_at = models.DateTimeField(auto_now=True)
+    alumnos_minimos = models.PositiveIntegerField()
+    profesor = models.ForeignKey(Profesor,)
+    curso = models.ForeignKey(Curso)
+
+    def __unicode__(self):
+        return self.curso.titulo + ' -------- ' + self.profesor.nombre + ' ' + self.profesor.apellidos
+
+
+class Edicion(models.Model):
+    numero =  models.CharField(max_length=20)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    profesor = models.ForeignKey(Profesor,related_name='ediciones')
+    curso = models.ForeignKey(Curso,related_name='ediciones')
+
+    def __unicode__(self):
+        return self.numero + ' - ' + self.curso.titulo
+
+    def codigo(self):
+        return self.curso.numero+'-'+self.numero
+
+    def alumnos_minimos(self):
+        return Posibilidad.objects.get(profesor_id = self.profesor_id,curso_id = self.curso_id).alumnos_minimos
